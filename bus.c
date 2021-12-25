606f1918606e98da04ae43330024c3aaa5695ef3
sara1301 <ghavams2001@gmail.com>
2021-12-25 11:25:58 +0330

    first commit

---
+++ bus.c
@@ -0,0 +1,23 @@
+// Fill in empty functions and defintions
+
+#include "pintos_headers.h"
+
+struct station {
+
+};
+
+void station_setup(struct station *station) {
+
+}
+
+void bus_load_passengers(struct station *station, int count) {
+
+}
+
+void passenger_waitfor_bus(struct station *station) {
+
+}
+
+void passenger_on_board(struct station *station) {
+
+}
